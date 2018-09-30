c69c7955a23a078443965283bb91bf59dee306db
mas9612 <mas9612@gmail.com>
2018-09-30 17:38:35 +0900

    split http handler into handler.go

--- server.go
+++ server.go
@@ -3,7 +3,6 @@ package trainwatcher
 import (
 	"encoding/json"
 	"io/ioutil"
-	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -65,18 +64,6 @@ func newRouter() *mux.Router {
 	return mux
 }
 
-func (w Watcher) ServeHTTP(writer http.ResponseWriter, req *http.Request) {
-	w.mux.ServeHTTP(writer, req)
-}
-
-type errorResponse struct {
-	Message string `json:"message"`
-}
-
-type health struct {
-	Status string `json:"status"`
-}
-
 func notFoundHandler(w http.ResponseWriter, r *http.Request) {
 	sendJSON(w, http.StatusNotFound, errorResponse{
 		Message: "Not Found",
@@ -89,92 +76,6 @@ func methodNotAllowedHandler(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
-func healthHandler(w http.ResponseWriter, r *http.Request) {
-	health := health{
-		Status: "healthy",
-	}
-
-	resp, err := http.Get(apiEndpoint)
-	if err != nil {
-		health.Status = "unhealthy"
-	}
-	defer resp.Body.Close()
-
-	if err == nil && resp.StatusCode != http.StatusOK {
-		health.Status = "unhealthy"
-	}
-
-	sendJSON(w, http.StatusOK, health)
-}
-
-type delay struct {
-	Name           string
-	Company        string
-	LastUpdatedGMT int
-	Source         string
-}
-
-type delaying struct {
-	Name    string
-	Company string
-}
-
-func delayHandler(w http.ResponseWriter, r *http.Request) {
-	resp, err := http.Get(apiEndpoint)
-	if err != nil {
-		log.Println(err)
-		msg := errorResponse{
-			Message: "Internal Server Error",
-		}
-		sendJSON(w, http.StatusInternalServerError, msg)
-		return
-	}
-	defer resp.Body.Close()
-
-	var statuses []delay
-	decoder := json.NewDecoder(resp.Body)
-	if err := decoder.Decode(&statuses); err != nil {
-		log.Println(err)
-		msg := errorResponse{
-			Message: "Internal Server Error",
-		}
-		sendJSON(w, http.StatusInternalServerError, msg)
-		return
-	}
-
-	result := make([]delaying, 0, len(statuses))
-	for _, status := range statuses {
-		for _, w := range watching {
-			if status.Company == w {
-				r := delaying{
-					Name:    status.Name,
-					Company: status.Company,
-				}
-				result = append(result, r)
-			}
-		}
-	}
-
-	sendJSON(w, http.StatusOK, result)
-}
-
-type watchingCompanies struct {
-	Companies []string `json:"companies"`
-}
-
-func watchingHandler(w http.ResponseWriter, r *http.Request) {
-	watchingCompanies := watchingCompanies{
-		Companies: watching,
-	}
-	sendJSON(w, http.StatusOK, watchingCompanies)
-}
-
-func sendJSON(w http.ResponseWriter, status int, v interface{}) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(status)
-	encoder := json.NewEncoder(w)
-	if err := encoder.Encode(v); err != nil {
-		log.Println("Failed to send JSON")
-		log.Printf("JSON: %+v\n", v)
-	}
+func (w Watcher) ServeHTTP(writer http.ResponseWriter, req *http.Request) {
+	w.mux.ServeHTTP(writer, req)
 }
